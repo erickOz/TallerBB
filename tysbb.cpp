20767a01885f608fc1ea4646e963376225c0abbe
alulab14 <alulab14@inf.pucp.edu.pe>
2019-03-26 15:57:33 -0500

    asdf

--- tysbb.cpp
+++ tysbb.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main{
-
+//Este es un comentario
 	int a,b;
 	b = 0;
 	for (int i = 0;i<100;i++){
 		printf("BRR");
 		b += 2;
-		prinf("%d",b);	
+		prinf("%d",b);
 	}
 
 }
